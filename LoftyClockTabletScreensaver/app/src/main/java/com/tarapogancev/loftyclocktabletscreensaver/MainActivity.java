fc102c271ffe54f4f7d219f8a87897aaf3b4f342
Tara Pogančev <tarapogancev@gmail.com>
2022-10-14 14:54:44 +0200

    feat: new icon, charging service made into bluetooth foreground service

--- LoftyClockTabletScreensaver/app/src/main/java/com/tarapogancev/loftyclocktabletscreensaver/MainActivity.java
+++ LoftyClockTabletScreensaver/app/src/main/java/com/tarapogancev/loftyclocktabletscreensaver/MainActivity.java
@@ -1,56 +1,25 @@
 package com.tarapogancev.loftyclocktabletscreensaver;
 
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
-import androidx.lifecycle.ProcessLifecycleOwner;
 
-import android.annotation.SuppressLint;
-import android.bluetooth.BluetoothA2dp;
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothGatt;
-import android.bluetooth.BluetoothGattCallback;
-import android.bluetooth.BluetoothProfile;
-import android.bluetooth.BluetoothSocket;
-import android.content.BroadcastReceiver;
-import android.content.ComponentName;
-import android.content.Context;
 import android.content.Intent;
-import android.content.ServiceConnection;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.IBinder;
-import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.tarapogancev.loftyclocktabletscreensaver.service.AppLifecycleObserver;
 import com.tarapogancev.loftyclocktabletscreensaver.service.BluetoothService;
-import com.tarapogancev.loftyclocktabletscreensaver.service.ChargingService;
 
-import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.net.Socket;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Date;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
-import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView time;
     Button buttonBack, buttonDisconnect;
-    BluetoothService bluetoothService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,11 +37,6 @@ public class MainActivity extends AppCompatActivity {
 
         writeTime();
         hideScreenElements();
-
-        AppLifecycleObserver appLifecycleObserver = new AppLifecycleObserver();
-        ProcessLifecycleOwner.get().getLifecycle().addObserver(appLifecycleObserver);
-        Intent chargingService = new Intent(getApplicationContext(), ChargingService.class);
-        //this.startService(chargingService);
 
         Intent bluetoothService = new Intent(getApplicationContext(), BluetoothService.class);
         ContextCompat.startForegroundService(this, bluetoothService);
--- LoftyClockTabletScreensaver/app/src/main/java/com/tarapogancev/loftyclocktabletscreensaver/service/BluetoothService.java
+++ LoftyClockTabletScreensaver/app/src/main/java/com/tarapogancev/loftyclocktabletscreensaver/service/BluetoothService.java
@@ -9,15 +9,22 @@ import android.app.Service;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
 import android.os.Build;
 import android.os.IBinder;
 import android.util.Log;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.core.app.NotificationCompat;
 
 import com.tarapogancev.loftyclocktabletscreensaver.MainActivity;
+import com.tarapogancev.loftyclocktabletscreensaver.R;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -34,6 +41,9 @@ public class BluetoothService extends Service {
     static final UUID DOCK_UUID = UUID.fromString("931c7e8a-540f-4686-b798-e8df0a2ad9f7");
     public static final String CHANNEL_ID = "ForegroundServiceChannel";
 
+    private AppState lastState = AppState.NOT_CHARGING;
+    ChargeDetection chargeDetector;
+
     public BluetoothService() {
     }
 
@@ -49,15 +59,26 @@ public class BluetoothService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        chargeDetector = new ChargeDetection();
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         createNotificationChannel();
 
+        IntentFilter filter = new IntentFilter();
+        filter.setPriority(IntentFilter.SYSTEM_HIGH_PRIORITY);
+        filter.addAction(Intent.ACTION_POWER_CONNECTED);
+        filter.addAction(Intent.ACTION_POWER_DISCONNECTED);
+        this.registerReceiver(chargeDetector, filter);
+        startListeningForCharging();
+
         Intent notificationIntent = new Intent(this, MainActivity.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this,
-                0, notificationIntent, 0);
+        notificationIntent.setAction(Intent.ACTION_MAIN);
+        notificationIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,0, notificationIntent, PendingIntent.FLAG_IMMUTABLE);
+
         Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
-                .setContentTitle("Foreground Service")
-                .setContentText("Lofty Clock")
+                .setContentTitle("Lofty Clock Screensaver")
+                .setContentText("Tap to return to the application.")
+                .setSmallIcon(R.drawable.ic_launcher_round)
                 .setContentIntent(pendingIntent)
                 .setSilent(true)
                 .build();
@@ -75,6 +96,32 @@ public class BluetoothService extends Service {
             );
             NotificationManager manager = getSystemService(NotificationManager.class);
             manager.createNotificationChannel(serviceChannel);
+        }
+    }
+
+    private void startListeningForCharging() {
+        IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        Intent batteryStatus = this.registerReceiver(null, ifilter);
+        int status = batteryStatus.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
+        boolean isCharging = status == BatteryManager.BATTERY_STATUS_CHARGING ||
+                status == BatteryManager.BATTERY_STATUS_FULL;
+
+        if (isCharging) {
+            if (lastState == AppState.NOT_CHARGING) {
+                try {
+                    Intent intent = new Intent(this, MainActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    //startActivity(intent);
+                    connectToAudioDevice();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            lastState = AppState.CHARGING;
+        } else {
+            lastState = AppState.NOT_CHARGING;
+            disconnectFromAudioDevice();
         }
     }
 
@@ -107,6 +154,20 @@ public class BluetoothService extends Service {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(chargeDetector);
+    }
+}
+
+class ChargeDetection extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        Intent batteryStatus = context.registerReceiver(null, ifilter);
     }
 
 }
